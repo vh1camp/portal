e8076fb18809623262304625ffb93168c5813bb8
VH1 Admin <admin@vh1basketball.com>
2026-06-02 08:09:22 -0600

    Login: add spinner to camper + staff/clinician buttons too
    Match the admin button behavior so users get visible feedback while
    the auth query is in flight, even if it is fast.
    
    SW: bump cache to v12.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Tapping the notification focuses (or opens) the portal.
  */
 
-const CACHE='vh1-camp-v11';
+const CACHE='vh1-camp-v12';
 const ASSETS=['./','./index.html'];
 
 self.addEventListener('install',e=>{
